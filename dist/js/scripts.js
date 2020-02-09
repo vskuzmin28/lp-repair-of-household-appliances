5448c03bc3668010e53217c63b9abc9d425f8cb0
vskuzmin28 <vskuzmin28@gmail.com>
2020-02-09 14:45:21 +0300

    updated project

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -380,3 +380,21 @@ $(function() {
   ymap();
 
 });
+
+let d = new Date(); // 30 января 2014
+
+function formatDate(date) {
+
+  let dd = date.getDate();
+  if (dd < 10) dd = '0' + dd;
+
+  let mm = date.getMonth() + 1;
+  if (mm < 10) mm = '0' + mm;
+
+  let yy = date.getFullYear() % 100;
+  if (yy < 10) yy = '0' + yy;
+
+  return dd + '.' + mm + '.' + yy;
+}
+
+$('.currentDay').html(formatDate(d));
